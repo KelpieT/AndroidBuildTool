71bf7289f4fdb4bacf7b1adcd9a43f68ba6a5a7a
Andrey <40003779+KelpieT@users.noreply.github.com>
2023-11-07 19:41:58 +0200

    Version 1.0.0

---
+++ Editor/Scripts/AndroidBuildTool.cs
@@ -0,0 +1,153 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System;
+
+#if UNITY_EDITOR
+namespace Viter.BuildTools
+{
+    public class AndroidBuildTool
+    {
+        private const string CHEATS = "CHEATS";
+        private const string PATH_TO_BUILD_FOLDER = "../Build";
+        private static string pathToBuidVersion;
+
+        [MenuItem("Build/Build Android")]
+        public static void BuildAll()
+        {
+            pathToBuidVersion = Path.Combine(Application.dataPath, PATH_TO_BUILD_FOLDER, Application.version);
+
+            EnableCheats(true);
+            string fileformat = "apk";
+            string debug = "cheats";
+            EditorUserBuildSettings.buildAppBundle = false;
+            Build(fileformat, debug);
+
+            EnableCheats(false);
+            debug = "release";
+            Build(fileformat, debug);
+
+            fileformat = "aab";
+            EditorUserBuildSettings.buildAppBundle = true;
+            Build(fileformat, debug);
+
+            CreateZipFile();
+        }
+
+        [MenuItem("Build/Build Cheats APK")]
+        public static void BuildCheeatsApk()
+        {
+            pathToBuidVersion = Path.Combine(Application.dataPath, PATH_TO_BUILD_FOLDER, Application.version);
+
+            EnableCheats(true);
+            string fileformat = "apk";
+            string debug = "cheats";
+            EditorUserBuildSettings.buildAppBundle = false;
+            Build(fileformat, debug);
+        }
+
+        private static void CreateZipFile()
+        {
+            string zipPath = pathToBuidVersion + ".zip";
+            if (File.Exists(zipPath))
+            {
+                File.Delete(zipPath);
+            }
+            try
+            {
+                ZipFile.CreateFromDirectory(pathToBuidVersion, zipPath);
+            }
+            catch (Exception e)
+            {
+                Debug.LogWarning(e.Message);
+            }
+        }
+
+
+        [MenuItem("Build/EnableCheats")]
+        public static void EnableCheats()
+        {
+            EnableCheats(true);
+        }
+
+        [MenuItem("Build/DisableCheats")]
+        public static void DisableCheats()
+        {
+            EnableCheats(false);
+        }
+
+        private static void Build(string fileformat, string debug)
+        {
+            BuildPlayerOptions buildPlayerOptions = GetBuildOptions(fileformat, debug);
+
+
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log($"{debug} {fileformat}  succeeded: " + summary.totalSize + " bytes");
+            }
+
+            if (summary.result == BuildResult.Failed)
+            {
+                Debug.Log($"{debug} {fileformat} failed");
+            }
+        }
+
+
+        private static BuildPlayerOptions GetBuildOptions(string format, string debug)
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            string[] scenes = EditorBuildSettings.scenes.Where(x=>x.enabled).Select(x => x.path).ToArray();
+            buildPlayerOptions.scenes = scenes;
+            buildPlayerOptions.locationPathName = Path.Combine(pathToBuidVersion, $"{format}/{debug}/mahjong{Application.version}_{debug}.{format}");
+            buildPlayerOptions.target = BuildTarget.Android;
+            buildPlayerOptions.options = BuildOptions.None;
+            return buildPlayerOptions;
+        }
+
+        // [MenuItem("Build/TestEnableCheats")]
+        // public static void TestEnableCheats()
+        // {
+        //     EnableCheats(true);
+        // }
+
+        // [MenuItem("Build/TestDisableCheats")]
+        // public static void TestDisableCheats()
+        // {
+        //     EnableCheats(false);
+        // }
+
+
+        private static void EnableCheats(bool enable)
+        {
+            string defines = PlayerSettings.GetScriptingDefineSymbols(UnityEditor.Build.NamedBuildTarget.Android);
+            Debug.Log(defines);
+            string[] defs = defines.Split(';');
+            List<string> newDefs = defs.ToList();
+            if (enable)
+            {
+                if (!newDefs.Contains(CHEATS))
+                {
+                    newDefs.Add(CHEATS);
+                }
+            }
+            else
+            {
+                newDefs.Remove(CHEATS);
+            }
+            string newDefines = string.Join(';', newDefs);
+            Debug.Log(newDefines);
+            PlayerSettings.SetScriptingDefineSymbols(UnityEditor.Build.NamedBuildTarget.Android, newDefines);
+            Debug.Log(PlayerSettings.GetScriptingDefineSymbols(UnityEditor.Build.NamedBuildTarget.Android));
+            AssetDatabase.Refresh();
+            return;
+        }
+    }
+}
+#endif
